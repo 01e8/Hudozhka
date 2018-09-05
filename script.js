d321bb0f7e3176e390f7dbf0d30ee20e24910d55
golubev-og <34693908+golubev-og@users.noreply.github.com>
2018-09-06 01:37:49 +0300

    Файлы добавлены

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+ 
+    
+engage.onclick = function() {
+           
+            curtain.style.marginTop = '0px';
+            engage.style.marginTop = '-200px';
+            reengage.style.marginTop = '30px';
+}
+
+reengage.onclick = function() {
+           
+            curtain.style.marginTop = '-40px';
+            reengage.style.marginTop = '-200px';
+            engage.style.marginTop = '30px';
+
+}
+
+}
